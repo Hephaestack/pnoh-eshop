7d7e181744ff25de5d97687dcc79679d2feac535
zycon91 <fzikos@gmail.com>
2025-08-14 13:47:26 +0300

    add: crud endpoints for product

--- backend/db/schemas/product.py
+++ backend/db/schemas/product.py
@@ -17,9 +17,36 @@ class ProductSummary(BaseModel):
     class Config:
         from_attributes = True
 
+class ProductBase(BaseModel):
+    name: str
+    description: Optional[str]
+    price: float
+    category: Optional[Category] = None
+    sub_category: Optional[SubCategory] = None
+    image_url: Optional[List[str]] = None
+
+class ProductCreate(ProductBase):
+    pass
+
 class ProductImageOut(BaseModel):
     id: UUID
     big_image_url: Optional[str]
+
+    class Config:
+        from_attributes = True
+
+class ProductUpdateRequest(BaseModel):
+    name: Optional[str]
+    description: Optional[str]
+    price: Optional[float]
+    category: Optional[Category]
+    sub_category: Optional[SubCategory]
+    image_url: Optional[List[str]]
+
+class ProductOut(ProductBase):
+    id: UUID
+    created_at: datetime
+    updated_at: Optional[datetime] = None
 
     class Config:
         from_attributes = True
--- backend/routes/admin_panel.py
+++ backend/routes/admin_panel.py
@@ -1,11 +1,13 @@
+from uuid import UUID, uuid4
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from utils.database import get_db
-from db.models import Admin
+from db.models import Admin, Product
+from db.schemas.product import ProductCreate, ProductOut, ProductUpdateRequest
 from db.schemas.admin import AdminLogin
-from utils.admin_auth import create_access_token, verify_password
+from utils.admin_auth import create_access_token, verify_password, get_current_admin
 
 router = APIRouter()
 
@@ -44,3 +46,55 @@ def login_admin(
     )
 
     return response
+
+@router.post("/admin/products/", response_model=ProductOut, tags=["Admin Products"])
+def create_product(
+    product_data: ProductCreate,
+    db: Session = Depends(get_db),
+    admin: Admin = Depends(get_current_admin)
+):
+    new_product = Product(
+        id = uuid4(),
+        **product_data.model_dump()
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+
+    return new_product
+
+@router.put("/admin/products/{product_id}", response_model=ProductOut, tags=["Admin Products"])
+def update_product(
+    product_id: UUID,
+    product_data: ProductUpdateRequest,
+    db: Session = Depends(get_db),
+    admin: Admin = Depends(get_current_admin)
+):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found.")
+    
+    update_data = product_data.model_dump(exclude_unset = True)
+    for field, value in update_data.items():
+        setattr(product, field, value)
+
+    db.commit()
+    db.refresh(product)
+
+    return product
+
+@router.delete("/admin/products/{product_id}", tags=["Admin Products"])
+def delete_product(
+    product_id: str,
+    db: Session = Depends(get_db),
+    admin: Admin = Depends(get_current_admin)
+):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    db.delete(product)
+    db.commit()
+    
+    return {"detail": "Product deleted successfully"}
